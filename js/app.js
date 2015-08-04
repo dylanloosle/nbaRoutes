5c036647994fbc94975f76aa42abedc43223e3d2
Dylan Loosle <dylanloosle@gmail.com>
2015-08-03 19:21:44 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 var app = angular.module('nbaRoutes', ['ngRoute']);
 
 app.config(function($routeProvider, $httpProvider){
-  $httpProvider.interceptors.push('httpRequestInterceptor');
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+    $routeProvider
+        .when('/', {
+            templateUrl: 'js/home/homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+        .when('/teams/:team', {
+            templateUrl: '/Javascript/nbaRoutes/js/teams/teamTmpl.html',
+            controller: 'teamCtrl',
+            resolve: {
+                teamData: function(teamService){
+                    return teamService.getTeamData($route.current.params.team);
+                }
+            }
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
 
-  //router here
+
 });
+
+
+
+
+
+
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,57 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
+    this.addNewGame = function(gameObj){
+        var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+        if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+            gameObj.won = true;
+        }
+        else {
+            gameObj.won = false;
+        }
+        var dfd = $q.defer();
+        $http({
+            method: 'POST',
+            url: url,
+            data: gameObj
+
+        }).then(function(gameObj){
+            dfd.resolve(gameObj)
+        });
+
+        return dfd.promise;
+    };
+
+
+
+
+
+    this.getTeamData = function(team){
+        var dfd = $q.defer();
+        var url = 'https://api.parse.com/1/classes/' + team;
+        $http({
+            method: 'GET',
+            url: url
+        }).then(function(data){
+            var results = data.data.results;
+            var wins = 0;
+            var losses = 0;
+            for(var i = 0; i < results.length; i++){
+                if(results.won === true){
+                    wins++;
+                }
+                else {
+                    losses++;
+                }
+            }
+            results.wins = wins;
+            results.losses = losses;
+
+            dfd.resolve(results);
+        });
+
+
+        return dfd.promise;
+    }
 
 });
